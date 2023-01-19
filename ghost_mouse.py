2f18e801272d9816311fe35a8d31b5faf7184d57
WAFFLE_HAU5 <117693381+Waffle-Hau5@users.noreply.github.com>
2023-01-19 14:05:41 -0500

    Using PIL library now, not sure how much it changes.
    I'm currently getting, ERROR:  raise NotImplementedError('The confidence keyword argument is only available if OpenCV is installed.')
    NotImplementedError: The confidence keyword argument is only available if OpenCV is installed. Even though OpenCV is definitely in the venv and installed.

--- ghost_mouse.py
+++ ghost_mouse.py
@@ -2,64 +2,62 @@ import pyautogui
 import math
 import random
 import time
-
+from PIL import Image
 
 # Written in python 3.8, I intend on fleshing this out a bit, making this a bot to mine gold in a "time-sink" game.
 # Main function for mouse's movement, may make different variations of movement to throw the "bot detection" off.
 
 def move_mouse(image_file, click_at_location=None, grayscale=False, confidence=None):
+    # I'm not certain I need to use Pillow? but here it is.
+    image = Image.open(image_file)
+    location = pyautogui.locateOnScreen(image, grayscale=grayscale, confidence=confidence)
 
-    location = pyautogui.locateOnScreen(image_file, grayscale=grayscale, confidence=confidence)
+    # If the image_file is found, unpack the x and y coordinates of the top-left corner
+    # and the width and height of the image...
+    x, y, image_width, image_height = location
 
-    if not location:
-        print("Your reference was not found.. Try decreasing confidence.")
-    if location:
-        # If the image_file is found, unpack the x and y coordinates of the top-left corner
-        # and the width and height of the image...
-        x, y, image_width, image_height = location
+    # Instead of clicking on the corner of the image provided, it will randomly choose a location
+    # within the image bounds, somewhat like a human would.
 
-        # Instead of clicking on the corner of the image provided, it will randomly choose a location
-        # within the image bounds, somewhat like a human would.
+    x += random.uniform(0, image_width)
+    y += random.uniform(0, image_height)
 
-        x += random.uniform(0, image_width)
-        y += random.uniform(0, image_height)
+    # Gets the current position of the mouse...
+    start_x, start_y = pyautogui.position()
 
-        # Gets the current position of the mouse...
-        start_x, start_y = pyautogui.position()
+    # Calculate the distance between the starting and ending positions.
+    distance = math.sqrt((x - start_x) ** 2 + (y - start_y) ** 2)
 
-        # Calculate the distance between the starting and ending positions.
-        distance = math.sqrt((x - start_x) ** 2 + (y - start_y) ** 2)
+    # Setting random number of steps to take.
+    randomsteps = random.randint(1, 30)
+    steps = int(distance / randomsteps)
+    angle = math.atan2(y - start_y, x - start_x)
 
-        # Setting random number of steps to take.
-        randomsteps = random.randint(1, 30)
-        steps = int(distance / randomsteps)
-        angle = math.atan2(y - start_y, x - start_x)
+    # Set your max random deviation "angle" during mouse movement here..
+    max_deviation = 10
 
-        # Set your max random deviation "angle" during mouse movement here..
-        max_deviation = 10
+    # Here's the goods:
+    for i in range(steps):
 
-        # Here's the goods:
-        for i in range(steps):
+        # Calculate the x and y coordinates with an angle
+        x = start_x + i * 10 * math.cos(angle)
+        y = start_y + i * 10 * math.sin(angle)
 
-            # Calculate the x and y coordinates with an angle
-            x = start_x + i * 10 * math.cos(angle)
-            y = start_y + i * 10 * math.sin(angle)
+        # Added a random deviation to the x and y coordinates...
+        x += random.uniform(-max_deviation, max_deviation)
+        y += random.uniform(-max_deviation, max_deviation)
+        pyautogui.moveTo(x, y)
 
-            # Added a random deviation to the x and y coordinates...
-            x += random.uniform(-max_deviation, max_deviation)
-            y += random.uniform(-max_deviation, max_deviation)
-            pyautogui.moveTo(x, y)
+        # Added a random "human-like" pauses for the botto.
+        # Currently, 40% chance quick pause, 2% long pause... 
+        pause = random.randint(1, 100)
+        if pause > 60:
+            time.sleep(random.uniform(0.1, 0.5))
+        elif pause > 98:
+            time.sleep(random.uniform(10, 30))
 
-            # Added a random "human-like" pauses for the botto.
-            # Currently, 40% chance quick pause, 3% long pause...
-            pause = random.randint(1, 100)
-            if pause > 60:
-                time.sleep(random.uniform(0.1, 0.5))
-            elif pause > 97:
-                time.sleep(random.uniform(10, 30))
-
-        if click_at_location:
-            pyautogui.doubleClick((x, y))
+    if click_at_location:
+        pyautogui.doubleClick((x, y))
 
 
 # Execute function as such; greyscale is set as None but could be useful, no need to define.
